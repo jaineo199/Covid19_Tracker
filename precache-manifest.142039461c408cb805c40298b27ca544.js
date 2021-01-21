431e1943772f339ccbb6701b196a243f2afb54ed
jai <jai@jais-MacBook-Air.local>
2021-01-21 15:30:33 +0530

    Updates

---
+++ precache-manifest.142039461c408cb805c40298b27ca544.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6d4625149ac4ab1217f5a3da839061",
+    "url": "/Covid19_Tracker/index.html"
+  },
+  {
+    "revision": "7e8f788e9fde1b141df9",
+    "url": "/Covid19_Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "32556359649f969a9e88",
+    "url": "/Covid19_Tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "7e8f788e9fde1b141df9",
+    "url": "/Covid19_Tracker/static/js/2.df75d9a8.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid19_Tracker/static/js/2.df75d9a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32556359649f969a9e88",
+    "url": "/Covid19_Tracker/static/js/main.4b66cbbe.chunk.js"
+  },
+  {
+    "revision": "0e5e63aa091c01ffa09b",
+    "url": "/Covid19_Tracker/static/js/runtime-main.c5b62e94.js"
+  }
+]);
